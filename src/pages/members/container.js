cb2686b786187047d77d398b460070a9e3e5d919
Ale Soto <asoto@lanacion.com.ar>
2019-07-22 06:36:11 -0300

    REFACTORIZO CON HOOKS

--- src/pages/members/container.js
+++ src/pages/members/container.js
@@ -1,57 +1,66 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../index.css'
 import MemberComponent from './component';
-import consoleLogHelper from '../../helpers/console';
 import Database from '../../helpers/db';
+import consoleLogHelper from '../../helpers/console';
 
-export default class MemberContainer extends Component {
-
-    state = {
+//SI RECIBE PROPS COMO PARAMETRO TENGO TODOS LOS DATOS DEL DOM
+export default function MemberContainer(props) {
+    const [init, initChange] = useState({
         users: null,
-        fetched: null
-    }
+        fetched: false
+    });
 
-    getMembers = async () => {
+    const users = init.users;
+    const fetched = init.fetched;
+
+    async function getMembers() {
         if (localStorage.getItem("users") === null) {
-            this.setState({
+            initChange({
                 users: await Database("villanos"),
                 fetched: true
             })
             consoleLogHelper("red", "Dato obtenido de Firestore")
         } else {
-            this.setState({
+            initChange({
                 users: JSON.parse(localStorage.getItem("users")),
                 fetched: true
             })
             consoleLogHelper("green", "Dato obtenido de localStorage")
         }
     }
+    //CALLBACK: If present, effect will only activate if the values in the list change
+    //Ahora la suscripción solo se volverá a crear cuando cambie fetched
+    useEffect(() => {
+        getMembers();
+    }, fetched);
 
-    componentDidMount() {
-        this.getMembers();
+    //COMO HAGO ESTO?    
+    // const { users, fetched } = init
+
+    let contenido;
+    if (fetched) {
+        contenido = Object.keys(users).sort((a, b) => { return a - b }).map((item, key) => {
+            return (
+                <MemberComponent key={key} data={users[item]} />
+            )
+        });
+    } else {
+        contenido = <p>Loading...</p>
     }
 
-    render() {
-        const { users, fetched } = this.state
-        let contenido;
-        if (fetched) {
-            contenido = Object.keys(users).map((item, key) => {
-                return (
-                    <MemberComponent key={key} data={users[item]} />
-                )
-            });
-        } else {
-            contenido = <p>Loading...</p>
-        }
-        return (
-            <div className="App">
-                <h2>↳{this.props.location.pathname}</h2>
-                <div className="container mt-4">
-                    <div className="row" style={{justifyContent:"center"}}>
-                        {contenido}
-                    </div>
+    return (
+        <div className="App">
+            <h2>↳{props.location.pathname}</h2>
+            <div className="container mt-4">
+                <div className="row" style={{ justifyContent: "center" }}>
+                    {contenido}
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+
+
+
