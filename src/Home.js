9f2a9bf9772691686a7b36e1bd204c92e45cc844
asoto <alejandro.soto@bibamgroup.com>
2020-08-07 01:02:52 -0300

    agrego info

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,18 @@ export default function Home(props) {
         <br />• Familia
         <br />• Respeto
         <br />• Lealtad
-        </blockquote> 
+        </blockquote>
+        <hr/>
+        <div>
+          <p>INSTAGRAM OFICIAL: <a href="https://instagr.am/mauro.ponti">@beardedvillainsargentina</a>🇦🇷</p>
+        </div>
+        <div>
+          <p>🌟🌟🌟 CAPTAIN: <a href="https://instagr.am/mauro.ponti">@mauro.ponti</a></p>
+          <p>⭐⭐⭐CO-CAPTAIN: <a href="https://instagr.am/gonzalo.bv">@gonzalo.bv</a></p>
+          <p>SCOUT: <a href="https://instagr.am/cristofian">@cristofian</a></p>
+          <p>SECRETARY: <a href="https://instagr.am/il_tano_">@il_tano_</a></p>
+          <p>OFFICERS: <a href="https://instagr.am/pontihector">@pontihector</a> <a href="https://instagr.am/g.o.f_79"> @g.o.f_79</a></p>
+        </div>
     </div >
   )
 }
